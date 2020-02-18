e672393a5857c57675071f74b5d6721e55289ee6
kenny1504 <kennysaenz31@gmail.com>
2020-02-17 22:24:15 -0600

    Ventanas modales (Estudiante ), editar para quitar paginacion en estudiante (Queda ejempo si )

--- wwwroot/js/js-redes/Estudiante/Paginacion.js
+++ wwwroot/js/js-redes/Estudiante/Paginacion.js
@@ -1,4 +1,6 @@
-  var results = new Array(); //arreglo para almacenar datos
+//PAGINACION 
+
+/* var results = new Array(); //arreglo para almacenar datos
         $(document).ready(function () {
             //Paginacion para estudiantes
             $.ajax({  // ajax para para recuperar datos
@@ -12,39 +14,62 @@
                     
                     $('#pagination-container').pagination({ //declaracion de la paginacion
                         dataSource: results, //asignacion de datos para paginar
-                        pageSize: 10, //indica la cantidad a mostrar por cada Pagina
+                        pageSize: 15, //indica la cantidad a mostrar por cada Pagina
                         callback: function (data, pagination) { 
                             
                         var html = "<table class='table table-bordered table-striped display'><thead><tr><th>Codigo estudiante</th><th>Nombre completo</th><th>Sexo</th><th>Direccion</th><th>Tutor</th><th>Tutor Telefono</th></tr></thead><tbody>";
-                            $.each(data, function (index, item){
+                            $.each(data, function (index, element){
                                 
                                 html += '<tr>';
                                 html +=
-                                    '<td>' + item.codigo + '</td>'
-                                    + '<td>' + item.nombre + '</td>'
-                                    + '<td>' + item.sexo + '</td>'
-                                    + '<td>' + item.direccion + '</td>'
-                                    + '<td>' + item.tutor
-                                    + '<td style="padding-top:0.1%; padding-bottom:0.1%;"class= "hidden" id="'+item.idEstudiante+'" >'
-                                    + '<button class="btn btn-primary" onclick="ver_estudiante(this);" data-id="'+item.idEstudiante+'" id="Ver-estudiante">ver</button>'
-                                    + '<button class="btn btn-success " data-id="'+item.idEstudiante+'" data-idper="'+item.idPersona+'" onclick="editar_estudiante(this);" ><i class=" fa fa-fw fa-pencil"></i></button>'
-                                    + '<button class="btn btn-info" data-id="'+item.idEstudiante+'" onclick="eliminar_estudiante(this);"><i class="fa fa-fw fa-trash "></i></button>'
-                                    + '<i class="fa fa-angle-double-right pull-right" onclick="mostrar(this);" data-id="' +item.idEstudiante+'"></i>'
+                                    '<td>' + element.codigo + '</td>'
+                                    + '<td>' + element.nombre + '</td>'
+                                    + '<td>' + element.sexo + '</td>'
+                                    + '<td>' + element.direccion + '</td>'
+                                    + '<td>' + element.tutor
+                                    + '<td style="padding-top:0.1%; padding-bottom:0.1%;"class= "hidden" id="'+element.idEstudiante+'" >'
+                                    + '<button class="btn btn-primary" onclick="ver_estudiante(this);" data-id="'+element.idEstudiante+'" id="Ver-estudiante">ver</button>'
+                                    + '<button class="btn btn-success " data-id="'+element.idEstudiante+'" data-idper="'+element.idPersona+'" onclick="editar_estudiante(this);" ><i class=" fa fa-fw fa-pencil"></i></button>'
+                                    + '<button class="btn btn-info" data-id="'+element.idEstudiante+'" onclick="eliminar_estudiante(this);"><i class="fa fa-fw fa-trash "></i></button>'
+                                    + '<i class="fa fa-angle-double-right pull-right" onclick="mostrar(this);" data-id="' +element.idEstudiante+'"></i>'
                                     + '</td>'
-                                    + '<td id="'+item.idEstudiante+'a" >' +item.telefono_tutor+ '<i class="fa fa-angle-double-right pull-right" onclick="mostrar(this);" data-id="'+item.idEstudiante+'"></i> </td>';
+                                    + '<td id="'+element.idEstudiante+'a" >' +element.telefono_tutor+ '<i class="fa fa-angle-double-right pull-right" onclick="mostrar(this);" data-id="'+element.idEstudiante+'"></i> </td>';
                               html += '</tr>';
                             })
                             html += "</tbody></table>";
                             $('#estudiantes').html(html); //insertamos datos paginados en tabla
                         }
                     })
-                }
-
-             
-                               
-                        
-
-
+                }                                               
             })
-        });
+        }); */
 
+$(document).ready(function () {
+    //Paginacion para estudiantes
+    $.ajax({  // ajax para para recuperar datos
+        type: "GET",
+        url: "Estudiante/Datos",
+        success: function (data) {
+            var html = "<table class='table table-bordered table-striped display'><thead><tr><th>Codigo estudiante</th><th>Nombre completo</th><th>Sexo</th><th>Direccion</th><th>Tutor</th><th>Tutor Telefono</th></tr></thead><tbody>";
+            data.forEach(element => {
+
+                   html = '<tr>'
+                        + '<td>' + element.codigo + '</td>'
+                        + '<td>' + element.nombre + '</td>'
+                        + '<td>' + element.sexo + '</td>'
+                        + '<td>' + element.direccion + '</td>'
+                        + '<td>' + element.tutor
+                        + '<td style="padding-top:0.1%; padding-bottom:0.1%;"class= "hidden" id="' + element.idEstudiante + '" >'
+                        + '<button class="btn btn-primary" onclick="ver_estudiante(this);" data-id="' + element.idEstudiante + '" id="Ver-estudiante">ver</button>'
+                        + '<button class="btn btn-success " data-id="' + element.idEstudiante + '" data-idper="' + element.idPersona + '" onclick="editar_estudiante(this);" ><i class=" fa fa-fw fa-pencil"></i></button>'
+                        + '<button class="btn btn-info" data-id="' + element.idEstudiante + '" onclick="eliminar_estudiante(this);"><i class="fa fa-fw fa-trash "></i></button>'
+                        + '<i class="fa fa-angle-double-right pull-right" onclick="mostrar(this);" data-id="' + element.idEstudiante + '"></i>'
+                        + '</td>'
+                        + '<td id="' + element.idEstudiante + 'a" >' + element.telefono_tutor + '<i class="fa fa-angle-double-right pull-right" onclick="mostrar(this);" data-id="' + element.idEstudiante + '"></i> </td>'
+                        +'</tr>';
+                        +"</tbody></table>";
+                $('#estudiantes').append(html); //insertamos datos en tabla
+            })
+        }
+    })
+});
