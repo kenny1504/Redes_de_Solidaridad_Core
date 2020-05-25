842313b729b6857c38cfefcf4ff36cc2c352159e
kenny saenz <kennysaenz31@gmail.com>
2020-05-25 12:45:15 -0600

    Menu de Usuarios  Listos

--- wwwroot/js/Inicio.js
+++ wwwroot/js/Inicio.js
@@ -1,10 +1,10 @@
-﻿$(document).ready(function () {
+﻿$(document).ready(function () { //Construye el menu segun el usuario logueado 
 	var text = $('#rol_u').text()
 
 	if (text == "1 ") {
-		$('#rol_u').text("Super Usuario")//Pone tipo de usuario en sesion
+		$('#rol_u').text("Administrador de sistema")//Pone tipo de usuario en sesion
 
-		document.getElementById("menu_u").innerHTML = //inserta item Usuarios
+		document.getElementById("menu_u").innerHTML = //inserta item 
 			"<li class='header' > Menu Navegacion</li >"+
             "<li id='menu_usuarios' class='treeview'>"+//Menu Usuarios
                 "<a href='#'>"+
@@ -27,7 +27,7 @@
                     "</span>"+
                 "</a>"+
                 "<ul class='treeview-menu'>"+//Menu dentro de Aministras "Menu Grados"
-                    "<li class='treeview'>"+
+                    "<li class='treeview'>"+//Menu Grados
                         "<a href='#'>"+
                             "<i class='fa fa-circle-o text-aqua'></i> Grados"+
                             "<span class='pull-right-container'>"+
@@ -42,17 +42,121 @@
                             "</li>"+
                             "<li class='treeview'>"+
                                 "<li><a href=' method=' GET'><i class='fa fa-circle-o text-yellow'></i> Mostrar</a></li>"+
-                   " </li>"+
-                "</ul>"
+                        "</li></ul>" +
+                      "<li class='treeview'>" +//Menu Grupos
+                        "<a href='#'>"+
+                            "<i class='fa fa-circle-o text-aqua'></i> Grupos"+
+                            "<span class='pull-right-container'>"+
+                                "<i class='fa fa-angle-left pull-right'></i>"+
+                            "</span>"+
+                       " </a>"+
+                        "<ul class='treeview-menu'>"+
+                            "<li>"+
+                                "<a href='#' data-toggle='modal' data-target='' id='m'>"+
+                                    "<i class='fa fa-circle-o text-yellow'></i> Agregar"+
+                                "</a>"+
+                            "</li>"+
+                            "<li class='treeview'>"+
+                                "<li><a href=' method=' GET'><i class='fa fa-circle-o text-yellow'></i> Mostrar</a></li>"+
+                        "</li></ul>"+          
+                      "<li class='treeview'>" +//Menu Oficios
+                        "<a href='#'>"+
+                            "<i class='fa fa-circle-o text-aqua'></i> Oficios"+
+                            "<span class='pull-right-container'>"+
+                                "<i class='fa fa-angle-left pull-right'></i>"+
+                            "</span>"+
+                       " </a>"+
+                        "<ul class='treeview-menu'>"+
+                            "<li>"+
+                                "<a href='#' data-toggle='modal' data-target='' id='m'>"+
+                                    "<i class='fa fa-circle-o text-yellow'></i> Agregar"+
+                                "</a>"+
+                            "</li>"+
+                            "<li class='treeview'>"+
+                                "<li><a href=' method=' GET'><i class='fa fa-circle-o text-yellow'></i> Mostrar</a></li>"+
+                        "</li></ul>"+
+                      "<li class='treeview'>" +//Menu Parentescos
+                        "<a href='#'>"+
+                            "<i class='fa fa-circle-o text-aqua'></i> Parentescos"+
+                            "<span class='pull-right-container'>"+
+                                "<i class='fa fa-angle-left pull-right'></i>"+
+                            "</span>"+
+                       " </a>"+
+                        "<ul class='treeview-menu'>"+
+                            "<li>"+
+                                "<a href='#' data-toggle='modal' data-target='' id='m'>"+
+                                    "<i class='fa fa-circle-o text-yellow'></i> Agregar"+
+                                "</a>"+
+                            "</li>"+
+                            "<li class='treeview'>"+
+                                "<li><a href=' method=' GET'><i class='fa fa-circle-o text-yellow'></i> Mostrar</a></li>"+
+                        "</li></ul>" +           
+                      "<li class='treeview'>" +//Menu Secciones
+                        "<a href='#'>"+
+                            "<i class='fa fa-circle-o text-aqua'></i> Secciones"+
+                            "<span class='pull-right-container'>"+
+                                "<i class='fa fa-angle-left pull-right'></i>"+
+                            "</span>"+
+                       " </a>"+
+                        "<ul class='treeview-menu'>"+
+                            "<li>"+
+                                "<a href='#' data-toggle='modal' data-target='' id='m'>"+
+                                    "<i class='fa fa-circle-o text-yellow'></i> Agregar"+
+                                "</a>"+
+                            "</li>"+
+                            "<li class='treeview'>"+
+                                "<li><a href=' method=' GET'><i class='fa fa-circle-o text-yellow'></i> Mostrar</a></li>"+
+                        "</li></ul>"+                    
+                      "<li class='treeview'>" +//Menu Turnos
+                        "<a href='#'>"+
+                            "<i class='fa fa-circle-o text-aqua'></i> Turnos"+
+                            "<span class='pull-right-container'>"+
+                                "<i class='fa fa-angle-left pull-right'></i>"+
+                            "</span>"+
+                       " </a>"+
+                        "<ul class='treeview-menu'>"+
+                            "<li>"+
+                                "<a href='#' data-toggle='modal' data-target='' id='m'>"+
+                                    "<i class='fa fa-circle-o text-yellow'></i> Agregar"+
+                                "</a>"+
+                            "</li>"+
+                            "<li class='treeview'>"+
+                                "<li><a href=' method=' GET'><i class='fa fa-circle-o text-yellow'></i> Mostrar</a></li>"+
+                        "</li></ul>"+
+                    "</ul >" //Fin Menu Administra
                
 	}
     if (text == "2 ") {
         $('#rol_u').text("Docente")//Pone tipo de usuario en sesion
+        document.getElementById("menu_u").innerHTML = //inserta item Usuarios
+            "<li class='header' > Menu Navegacion</li >" +
+            "<li id='menu_usuarios' class='treeview'>"+//Menu Notas
+                "<a href='#'>"+
+                   "<i class='fa fa-user'></i> <span> Notas</span>"+
+                    "<span class='pull-right-container'>"+
+                        "<i class='fa fa-angle-left pull-right'></i>"+
+                    "</span>"+
+                "</a>"+
+                "<ul class='treeview-menu'>"+
+                    "<li><a href='#' ><i class='fa fa-circle-o text-aqua'></i> Agregar</a></li>"
+                "</ul>"+
+            "</li >" +
+                    "<li id='menu_usuarios' class='treeview'>"+//Menu Asistencia
+                "<a href='#'>"+
+                   "<i class='fa fa-user'></i> <span> Asistencia</span>"+
+                    "<span class='pull-right-container'>"+
+                        "<i class='fa fa-angle-left pull-right'></i>"+
+                    "</span>"+
+                "</a>"+
+                "<ul class='treeview-menu'>"+
+                    "<li><a href='#' ><i class='fa fa-circle-o text-aqua'></i> Agregar</a></li>"
+                "</ul>"+
+            "</li >" 
     }
 	if (text == "3 ") {
 		$('#rol_u').text("Institucion")//Pone tipo de usuario en sesion
 
-		document.getElementById("menu_u").innerHTML = //inserta item Usuarios
+		document.getElementById("menu_u").innerHTML = //inserta item 
 			"<li class='header' > Menu Navegacion</li >"+
             "<li id='menu_usuarios' class='treeview'>"+//Menu Usuarios
                 "<a href='#'>"+
