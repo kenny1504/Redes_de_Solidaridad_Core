ae9c5eee4a1ef793599f2caf143b1d801f16f1cc
kenny saenz <kennysaenz31@gmail.com>
2020-06-03 22:00:21 -0600

    Usuarios CRUD (Cuando este logueado un instituto)

--- wwwroot/js/js-redes/Usuario/Docente/Ver.js
+++ wwwroot/js/js-redes/Usuario/Docente/Ver.js
@@ -13,9 +13,9 @@
                     + '<td>' + element.nombre + '</td>'
                     + '<td>' + element.institucion + '</td>'
                     + '<td style="padding-top:0.1%; padding-bottom:0.1%; id="' + element.idUsuario + '" >'
-                    + '<a class="btn btn-primary" onclick=" " data-id="' + element.idUsuario + '" id="Ver-estudiante">ver</a>'
-                    + '<a class="btn btn-success" data-id="' + element.idUsuario + '" onclick=" " ><i class=" fa fa-fw fa-pencil"></i></a>'
-                    + '<a class="btn btn-info" data-id="' + element.idUsuario + '" onclick=" "><i class="fa fa-fw fa-trash "></i></a>'
+                    + '<a class="btn btn-primary" onclick="ver_docente(this);" data-id="' + element.idUsuario + '" id="Ver-estudiante">ver</a>'
+                    + '<a class="btn btn-success" data-id="' + element.idUsuario + '" onclick="editar_Usuario_D(this)" ><i class=" fa fa-fw fa-pencil"></i></a>'
+                    + '<a class="btn btn-info" data-id="' + element.idUsuario + '" onclick="eliminar_Usuario_Docente(this)"><i class="fa fa-fw fa-trash "></i></a>'
                     + '</tr>';
                 $('#Usuarios tbody ').append(html); //insertamos datos en tabla
             })
