54127aa12f8d317fe741f172280c53b2a5d406c4
kenny saenz <kennysaenz31@gmail.com>
2020-06-04 11:20:12 -0600

    Modificando CRUD (Usuario logueado)

---
+++ wwwroot/js/js-redes/Usuario/Docente/Agregar.js
@@ -0,0 +1,88 @@
+﻿
+function ingresar_Usuario_D() {
+
+    limpiar();
+    $('#ingresar_Docente2').modal('show'); // abre ventana modal
+
+    var intitucion = $("#id_u").text();
+
+    $('#Instituciones').val(intitucion); //ingresa id de la intitucion loqueada
+
+}
+
+function ingresar_usuario_docente() { //ajax para Registrar una Institucion
+
+    if ($('#CedulaU').val() != "" && $('#UsuarioU').val() != "" && $('#ContrasenaU').val() != "" && $('#Instituciones').val() != null) {
+
+        var tabla = $.trim($("#Usuarios").html());
+
+        $.ajax({
+            url: "Usuarios/Registro_Usuario_Docente", // URL del controlador
+            type: "POST", //tipo de metodo 
+            data: $('#ingresar_Docentes2').serialize(), // pasamos el id del formulario para poder usar campos en el controlador
+            success: function (data) {
+                var usuario = data; // varible que recive lo que retorna el controlador
+                if (usuario != -1) { // si la variable es 1 entonces el usuario existe
+
+                    if (usuario != 0) {//si retorna 0 es porque el numero de cedula no pertene a un docente de la institucion
+
+                        if (tabla != "") //si la tabla no esta vacia, inserta Datos
+                        {
+                            data.forEach(element => {   //recorre arreglo
+                                var html = '<tbody><tr>'
+                                    + '<td>' + element.user + '</td>'
+                                    + '<td>' + element.pass + '</td>'
+                                    + '<td>' + element.nombbre + '</td>'
+                                    + '<td>' + element.inst + '</td>'
+                                    + '<td style="padding-top:0.1%; padding-bottom:0.1%; id="' + element.id + '" >'
+                                    + '<a class="btn btn-primary" onclick="ver_estudiante(this);" data-id="' + element.id + '" id="Ver-estudiante">ver</a>'
+                                    + '<a class="btn btn-success" data-id="' + element.id + '" onclick="editar_Usuario_D(this)" ><i class=" fa fa-fw fa-pencil"></i></a>'
+                                    + '<a class="btn btn-info" data-id="' + element.id + '" onclick="eliminar_Usuario_Docente(this)"><i class="fa fa-fw fa-trash "></i></a>'
+                                    + '</tr>'
+                                    + '</tbody>';
+                                $('#Usuarios').append(html); //insertamos datos en tabla
+                            })
+                        }
+
+                        $("#exito").modal("show"); //abre modal de exito
+                        $("#ingresar_Docente2").modal("hide"); // cierra modal error
+                        $("#exito").fadeTo(2900, 500).slideUp(450, function () {// cierra la modal despues del tiempo determinado  
+                            $("#exito").modal("hide");
+                        }); // cierra modal 
+                    }
+                    else { // error el usuario que ingreso ya existe en esta institucion
+
+                        var error = "Error,La cedula que ingreso no pertence a un docente de esta institucion "
+                        $('#mensaje').text(error);   //manda el error a la modal
+                        $("#Mensaje-error").modal("show"); //abre modal de error
+                        $("#Mensaje-error").fadeTo(2900, 500).slideUp(450, function () {// cierra la modal despues del tiempo determinado  
+                            $("#Mensaje-error").modal("hide"); // cierra modal error
+                        });
+                    }
+
+                }
+                else // si el usuario no existe 
+                {
+                    var error = "Error, Favor Verifique los datos, el nombre Usuario que ingreso Ya existe"
+                    $('#mensaje').text(error);   //manda el error a la modal
+                    $("#Mensaje-error").modal("show"); //abre modal de error
+                    $("#Mensaje-error").fadeTo(2900, 500).slideUp(450, function () {// cierra la modal despues del tiempo determinado  
+                        $("#Mensaje-error").modal("hide"); // cierra modal error
+                    });
+                }
+
+            },
+            error: function (jqXHR, textStatus, errorThrown) // si el ajax presenta errores entonces muestra en el alert
+            {
+                alert('Error Conexion a base de datos / Error 5000'); // si el ajax contiene errores se muestran aqui
+            }
+        });
+    }
+}
+function limpiar() {
+
+    $('#CedulaU').val("");
+    $('#UsuarioU').val("");
+    $('#ContrasenaU').val("");
+    $('#Instituciones').val("");
+}
---
+++ wwwroot/js/js-redes/Usuario/Docente/Editar.js
@@ -0,0 +1,99 @@
+﻿var dat; var usuario; var inst;
+
+function editar_Usuario_D(button) {
+    dat = $(button).parents("tr"); //captura toda la fila donde se efectuo el click (Editar)
+    var ide = $(button).attr("data-id"); // captura el valor_id_grupo "id" 
+    limpiar();
+    $('#editar_Docente2').modal('show'); // abre ventana modal
+
+    $.ajax({ // ajax para cargar datos 
+        type: 'POST',
+        url: 'Usuarios/Docente', // ruta 
+        data: { id: ide }, // manda id  al controlador, 
+        dataType: "JSON",
+        success: function (data) {
+            //ciclo para recorrer el arreglo 
+            data.forEach(element => {
+
+                $('#Id_UsuarioD').val(element.idUsuario);
+                $('#CedulaU2').val(element.cedula);
+                $('#UsuarioU2').val(element.usuario);
+                $('#ContrasenaU2').val(element.contrasena);
+                $('#Instituciones2').val(element.institucion);
+            });
+
+        }
+    });//Fin ajax 
+}//fin del metodo
+
+
+function limpiar() { // limpia campos
+
+    $('#Id_UsuarioD').val("");
+    $('#CedulaU2').val("");
+    $('#UsuarioU2').val("");
+    $('#ContrasenaU2').val("");
+    $('#Instituciones2').val("");
+};
+
+
+function editar_Usuario_Docente() {
+
+    if ($('#CedulaU2').val() != "" && $('#UsuarioU2').val() != "" && $('#ContrasenaU2').val() != "" && $('#Instituciones2').val() != null) {
+
+        usuario = $('#Id_UsuarioD').val();
+
+        $.ajax({
+            type: 'POST',
+            url: 'Usuarios/Editar_Usuario_Docente', // ruta editar grado
+            data: $('#editar_Docentes2').serialize(), // manda el form donde se encuentra la modal
+            dataType: "JSON", // tipo de respuesta del controlador
+            success: function (data) {
+                if (data != -1) {
+                    if (data != 0) {//si retorna 0 es porque el numero de cedula no pertene a un docente de la institucion
+                        data.forEach(element => {   //recorre arreglo
+                            var datos = '<tr>'
+                                + '<td>' + element.user + '</td>'
+                                + '<td>' + element.pass + '</td>'
+                                + '<td>' + element.nombbre + '</td>'
+                                + '<td>' + element.inst + '</td>'
+                                + '<td style="padding-top:0.1%; padding-bottom:0.1%; id="' + usuario + '" >'
+                                + '<a class="btn btn-primary" onclick="ver_estudiante(this);" data-id="' + usuario + '" id="Ver-estudiante">ver</a>'
+                                + '<a class="btn btn-success" data-id="' + usuario + '" onclick="editar_Usuario_D(this)" ><i class=" fa fa-fw fa-pencil"></i></a>'
+                                + '<a class="btn btn-info" data-id="' + usuario + '" onclick="eliminar_Usuario_Docente(this)"><i class="fa fa-fw fa-trash "></i></a>'
+                                + '</tr>';// variable guarda los nuevos valores
+
+                            dat.replaceWith(datos); //reemplaza por los nuevos datos
+                        })
+                        $("#exito").modal("show"); //abre modal de exito
+                        $("#exito").fadeTo(2000, 500).slideUp(450, function () {   // cierra la modal despues del tiempo determinado  
+                            $("#exito").modal("hide"); // cierra modal
+
+                        });
+                        limpiar();
+                        $('#editar_Docente2').modal("hide"); // cierra modal
+                    }
+                    else { // error el usuario que ingreso ya existe en esta institucion
+
+                        var error = "Error,La cedula que ingreso no pertence a un docente de la institucion seleccionada"
+                        $('#mensaje').text(error);   //manda el error a la modal
+                        $("#Mensaje-error").modal("show"); //abre modal de error
+                        $("#Mensaje-error").fadeTo(2900, 500).slideUp(450, function () {// cierra la modal despues del tiempo determinado  
+                            $("#Mensaje-error").modal("hide"); // cierra modal error
+                        });
+                    }
+
+                }
+                else // si el usuario no existe 
+                {
+                    var error = "Error, Favor Verifique los datos, el nombre Usuario que ingreso Ya existe"
+                    $('#mensaje').text(error);   //manda el error a la modal
+                    $("#Mensaje-error").modal("show"); //abre modal de error
+                    $("#Mensaje-error").fadeTo(2900, 500).slideUp(450, function () {// cierra la modal despues del tiempo determinado  
+                        $("#Mensaje-error").modal("hide"); // cierra modal error
+                    });
+                }
+            }
+        });
+    }
+}
