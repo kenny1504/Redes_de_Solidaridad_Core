b2f7c32a2260642fd86984db016444939ab8b58c
kenny saenz <kennysaenz31@gmail.com>
2020-06-05 10:28:46 -0600

    Servicio Cantidad de estudiantes por grupos segun una institucion

--- Controllers/EstudiantesWS.cs
+++ Controllers/EstudiantesWS.cs
@@ -20,6 +20,8 @@ namespace Redes_De_Solidaridad.Controllers
                 _context = context;
             }
 
+
+            //Servicio para recuperar la cantidad de estudiantes segun la institucion 
             [HttpGet("institucion/grado")]
 
             public async Task<ActionResult<List<Estudiantes_grado>>> Estudiantes_grados(Busqueda inst)
@@ -32,7 +34,7 @@ namespace Redes_De_Solidaridad.Controllers
                         join item3 in _context.Personas.ToList() on item2.PersonasId equals item3.Id
                         join item4 in _context.Institucion.ToList() on item3.IdInstitucion equals item4.Id
                         join item5 in _context.Gradoacademico.ToList() on item6.GradoAcademicoId equals item5.Id
-                        where item4.Id==inst.Id
+                        where item4.Id==inst.Id && item1.Fecha.Year==DateTime.Today.Year //valida que es año sea igual al año actual
                         group item5 by new { item5.Id, item5.Grado } into grados
                         select new Estudiantes_grado
                         {
@@ -41,8 +43,37 @@ namespace Redes_De_Solidaridad.Controllers
 
                         }).ToList();
 
-            return data;
+              return data;
  
             }
-        }
+
+            //Servicio para recuperar la cantidad de grupos segun un grado segun la institucion 
+            [HttpGet("institucion/grupos")]
+
+            public async Task<ActionResult<List<Gruposws>>> grupos_grados(Grupos_ws inst)
+            {
+                var data = new List<Gruposws>();
+
+                data = (from item6 in _context.Ofertas.ToList()
+                        join item1 in _context.Matriculas.ToList() on item6.Id equals item1.OfertasId
+                        join item2 in _context.Estudiantes.ToList() on item1.EstudiantesId equals item2.Id
+                        join item3 in _context.Personas.ToList() on item2.PersonasId equals item3.Id
+                        join item4 in _context.Institucion.ToList() on item3.IdInstitucion equals item4.Id
+                        join item5 in _context.Gradoacademico.ToList() on item6.GradoAcademicoId equals item5.Id
+                        join item7 in _context.Grupos.ToList() on item6.GruposId equals item7.Id
+                        where item4.Id == inst.institucion && item5.Id==inst.Grado && item1.Fecha.Year == DateTime.Today.Year //valida que es año sea igual al año actual
+                        group item7 by new { item7.Id, item7.Grupo } into grupo
+                        select new Gruposws
+                        {
+                            Grupo = grupo.Key.Grupo,
+                            Cantidad = grupo.Count()
+
+                        }).ToList();
+
+                return data;
+
+            }
+
+
+    }
     }
--- Models/Grupos.cs
+++ Models/Grupos.cs
@@ -8,4 +8,14 @@ namespace Redes_De_Solidaridad.Models
         public int Id { get; set; }
         public string Grupo { get; set; }
     }
+    public class Gruposws // clase que Retorna Datos
+    {
+        public int Cantidad { get; set; }
+        public string Grupo { get; set; }
+    }
+    public class Grupos_ws // clase que recive Datos
+    {
+        public int institucion { get; set; }
+        public int Grado { get; set; }
+    }
 }
