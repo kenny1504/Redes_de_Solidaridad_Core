5812d360e214ef62a9c31c7444050ef72195dac0
kenny saenz <kennysaenz31@gmail.com>
2020-06-08 22:54:14 -0600

    Avanzando con Dashboard para Administrador

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Redes_De_Solidaridad.Context;
+
+namespace Redes_De_Solidaridad.Controllers
+{
+    public class DashboardController : Controller
+    {
+
+        private readonly CentrosEscolares _context;
+
+        public DashboardController(CentrosEscolares context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> TotalEstudiantes()
+        {
+            var estudiantes = _context.Estudiantes.Count();
+
+            return Json(estudiantes);
+        }
+        public async Task<IActionResult> TotalInstituciones()
+        {
+            var institucion = _context.Institucion.Count();
+
+            return Json(institucion);
+        }
+        public async Task<IActionResult> TotalDocente()
+        {
+            var docente = _context.Docentes.Count();
+
+            return Json(docente);
+        }
+        public async Task<IActionResult> TotalMatriculas()
+        {
+            var docente = _context.Matriculas.Where(x => x.Fecha.Year == DateTime.Today.Year).Count();
+
+            return Json(docente);
+        }
+        public async Task<IActionResult> Total_Estudiantes()
+        {
+            var selec = (from i in _context.Institucion
+                         join d in _context.Detallematriculainstitucion on i.Id equals d.IdInstitucion
+                         join m in _context.Matriculas on d.IdMatricula equals m.Id
+                         where m.Fecha.Year==DateTime.Today.Year
+                         orderby i.Id descending
+                         group i by new {i.Nombre,i.Id} into institucion
+                         select new
+                         {
+                            Nombre=institucion.Key.Nombre,
+                            Cantidad=institucion.Count()
+                         }).Take(4); //Consulta para recuperar los ultimos  4 registros de las instituciones y su cantidad de matriculas
+
+            return Json(selec);
+        }
+
+
+    }
+}
---
+++ wwwroot/js/js-redes/Dashboard_Admin.js
@@ -0,0 +1,80 @@
+﻿$(document).ready(function () {
+    var TotalMatriculas;
+
+    $.ajax({  // ajax para para recuperar datos de Usuarios Docentes
+        type: "GET",
+        url: "Dashboard/TotalDocente",
+        success: function (data) {
+                $('#Total_Docentes').text(data);
+        }
+    })
+    $.ajax({  // ajax para para recuperar datos de instituciones registradas
+        type: "GET",
+        url: "Dashboard/TotalInstituciones",
+        success: function (data) {
+            $('#Total_Institucion').text(data);
+        }
+    })
+    $.ajax({  // ajax para para recuperar datos de instituciones Estudiantes
+        type: "GET",
+        url: "Dashboard/TotalEstudiantes", 
+        success: function (data) {
+            $('#Total_Estudiantes').text(data);
+        }
+    })
+    $.ajax({  // ajax para para recuperar datos de instituciones Estudiantes
+        type: "GET",
+        url: "Dashboard/TotalMatriculas",
+        success: function (data) {
+            TotalMatriculas = data;
+            $('#Total_Matriculas').text(data);
+        }
+    })
+    var i = 0; var style;
+    $.ajax({  // ajax para para recuperar datos de instituciones Estudiantes
+        type: "GET",
+        url: "Dashboard/Total_Estudiantes",
+        success: function (data) {
+            data.forEach(element => {
+                i++;
+                if (i == 1) {
+                    style = (element.cantidad * 100)/TotalMatriculas;
+                    $('#institucion1').text(element.nombre);
+                    var cifra = document.getElementById('institucion_Valor1');
+                    cifra.innerHTML = '<b>' + element.cantidad + '</b>/' + TotalMatriculas;
+                    var intro = document.getElementById('institucion_style1');
+                    intro.style.width = style.toString()+'%';
+                }
+                if (i == 2) {
+                    style = (element.cantidad * 100)/TotalMatriculas;
+                    $('#institucion2').text(element.nombre);
+                    var cifra = document.getElementById('institucion_Valor2');
+                    cifra.innerHTML = '<b>' + element.cantidad + '</b>/' + TotalMatriculas;
+                    var intro = document.getElementById('institucion_style2');
+                    intro.style.width = style.toString() + '%';
+                }
+                if (i == 3) {
+                    style = (element.cantidad * 100)/TotalMatriculas;
+                    $('#institucion3').text(element.nombre);
+                    var cifra = document.getElementById('institucion_Valor3');
+                    cifra.innerHTML = '<b>' + element.cantidad + '</b>/' + TotalMatriculas;
+                    var intro = document.getElementById('institucion_style3');
+                    intro.style.width = style.toString() + '%';
+                }
+                if (i == 4) {
+                    style = (element.cantidad * 100)/TotalMatriculas;
+                    $('#institucion4').text(element.nombre);
+                    var cifra = document.getElementById('institucion_Valor4');
+                    cifra.innerHTML = '<b>' + element.cantidad + '</b>/' + TotalMatriculas;
+                    var intro = document.getElementById('institucion_style4');
+                    intro.style.width = style.toString() + '%';;
+                }
+                
+            })
+        }
+    })
+
+
+
+       
+});
