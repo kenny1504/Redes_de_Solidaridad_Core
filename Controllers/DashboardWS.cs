28540a31c861400047e3505674a14028174596eb
kenny saenz <kennysaenz31@gmail.com>
2020-06-11 16:22:46 -0600

    Dashboard ADMIN Y INSTITUCION, Servicio para llenar grafico (Usuario Institucion)

--- Controllers/DashboardWS.cs
+++ Controllers/DashboardWS.cs
@@ -20,7 +20,7 @@ namespace Redes_De_Solidaridad.Controllers
             _context = context;
         }
 
-        [HttpGet("Dashboard/Admin")] //Servicio para llenar grafico
+        [HttpGet("Dashboard/Admin")] //Servicio para llenar grafico Usuario Administrador
         public async Task<ActionResult<List<Estudiantes_grado>>> Total_Grados()
         {
             var Grados = new List<Estudiantes_grado>(); //Lista de tipo grado
@@ -35,6 +35,26 @@ namespace Redes_De_Solidaridad.Controllers
                              Grado = grados.Key.Grado,
                              Cantidad = grados.Count()
                          }).ToList(); //Consulta para recuperar 
+
+            return Grados;
+        }
+        [HttpGet("Dashboard/Institucion")] //Servicio para llenar grafico Usuario Institucion
+        public async Task<ActionResult<List<Estudiantes_grado>>> Total_Grados(Busqueda inst)
+        {
+            var Grados = new List<Estudiantes_grado>(); //Lista de tipo grado
+
+            Grados = (from gr in _context.Gradoacademico
+                      join o in _context.Ofertas on gr.Id equals o.GradoAcademicoId
+                      join m in _context.Matriculas on o.Id equals m.OfertasId
+                      join e in _context.Estudiantes on m.EstudiantesId equals e.Id
+                      join p in _context.Personas on e.PersonasId equals p.Id
+                      where m.Fecha.Year == DateTime.Today.Year && p.IdInstitucion == inst.Id
+                      group gr by new { gr.Grado, gr.Id } into grados
+                      select new Estudiantes_grado
+                      {
+                          Grado = grados.Key.Grado,
+                          Cantidad = grados.Count()
+                      }).ToList(); //Consulta para recuperar 
 
             return Grados;
         }
