6d960918c8ecc7fea9b9f607cc32ee60e5f154e1
kenny1504 <kennysaenz31@gamil.com>
2020-07-04 17:24:09 -0600

    Servicio Editar Usuario (Institucion)

--- Controllers/UsuariosWS.cs
+++ Controllers/UsuariosWS.cs
@@ -14,7 +14,7 @@ namespace Redes_De_Solidaridad.Controllers
     [Route("api/[controller]")]
     [ApiController]
 
-    
+
     public class UsuariosWS : ControllerBase
     {
 
@@ -32,20 +32,20 @@ namespace Redes_De_Solidaridad.Controllers
 
             var data = new List<usuariosWS>();
 
-             data = (from item in _context.Usuarios.ToList()
-                        join item2 in _context.Institucion.ToList() on item.IdInstitucion equals item2.Id
-                        join item3 in _context.Personas.ToList() on item2.Id equals item3.IdInstitucion
-                        where item.Cedula == item3.Cedula && item.IdInstitucion==inst.Id
-                        select new usuariosWS
-                        {
-                                NombreDeUsuario =item.Usuario,
-                                Nombre= item3.Nombre + " " + item3.Apellido1 + " " + item3.Apellido2,
-                                Id=item.Id,
-                                tipo=2,
-                                Cedula=item.Cedula,
-                               Institucion=item2.Nombre
+            data = (from item in _context.Usuarios.ToList()
+                    join item2 in _context.Institucion.ToList() on item.IdInstitucion equals item2.Id
+                    join item3 in _context.Personas.ToList() on item2.Id equals item3.IdInstitucion
+                    where item.Cedula == item3.Cedula && item.IdInstitucion == inst.Id
+                    select new usuariosWS
+                    {
+                        NombreDeUsuario = item.Usuario,
+                        Nombre = item3.Nombre + " " + item3.Apellido1 + " " + item3.Apellido2,
+                        Id = item.Id,
+                        tipo = 2,
+                        Cedula = item.Cedula,
+                        Institucion = item2.Nombre
 
-                        }).ToList();
+                    }).ToList();
 
             return data;
         }
@@ -62,15 +62,15 @@ namespace Redes_De_Solidaridad.Controllers
                     where item.Cedula == u.Cedula
                     select new Personas
                     {
-                             Cedula=item3.Cedula,
-                             Nombre = item3.Nombre,
-                            Apellido1 = item3.Apellido1,
-                            Apellido2 = item3.Apellido2,
-                            Sexo = item3.Sexo,
-                            Direccion = item3.Direccion,
-                            Correo = item3.Correo,
-                            Telefono = item3.Telefono,
-                            FechaNacimiento = item3.FechaNacimiento
+                        Cedula = item3.Cedula,
+                        Nombre = item3.Nombre,
+                        Apellido1 = item3.Apellido1,
+                        Apellido2 = item3.Apellido2,
+                        Sexo = item3.Sexo,
+                        Direccion = item3.Direccion,
+                        Correo = item3.Correo,
+                        Telefono = item3.Telefono,
+                        FechaNacimiento = item3.FechaNacimiento
 
                     }).ToList();
 
@@ -81,17 +81,17 @@ namespace Redes_De_Solidaridad.Controllers
         {
             usuariosWS r;
             userview data = (from item in _context.Personas //verifica si la cedula ingresada pertenece a un docente de ka institucion
-                        join item2 in _context.Docentes on item.Id equals item2.PersonasId
-                        join item3 in _context.Institucion on item.IdInstitucion equals item3.Id
-                        where item.Cedula == docent.Cedula && item.IdInstitucion == docent.IdInstitucion
-                        select new userview
-                        {
-                            username = item.Nombre + " " + item.Apellido1 + item.Apellido2,
-                            password = item3.Nombre
+                             join item2 in _context.Docentes on item.Id equals item2.PersonasId
+                             join item3 in _context.Institucion on item.IdInstitucion equals item3.Id
+                             where item.Cedula == docent.Cedula && item.IdInstitucion == docent.IdInstitucion
+                             select new userview
+                             {
+                                 username = item.Nombre + " " + item.Apellido1 + item.Apellido2,
+                                 password = item3.Nombre
 
-                        }).FirstOrDefault();
+                             }).FirstOrDefault();
 
-            if (data!=null)
+            if (data != null)
             {
                 var user = _context.Usuarios.Where(x => x.Usuario == docent.Usuario && x.IdInstitucion == docent.IdInstitucion).FirstOrDefault(); //verifica si el nombre de usuario existe en la institucion
 
@@ -119,9 +119,9 @@ namespace Redes_De_Solidaridad.Controllers
                 }
                 else
                 {
-                    r =new usuariosWS 
+                    r = new usuariosWS
                     {
-                         Id = -1,
+                        Id = -1,
                         Nombre = "Ya existe",
                         NombreDeUsuario = "Ya existe",
                         tipo = -1,
@@ -130,7 +130,7 @@ namespace Redes_De_Solidaridad.Controllers
                     };
                     return r;// error el usuario que ingreso ya existe en esta institucion
                 }
-                   
+
 
             }
             else
@@ -146,23 +146,23 @@ namespace Redes_De_Solidaridad.Controllers
                 };
                 return r; //si retorna 0 es porque el numero de cedula no pertene a un docente de la institucion
             }
-               
+
         }
 
 
         [HttpPut("editarUsuario")]
-        public async Task<int> Editar_Usuario(Usuarios usuarios ) //metodo para actualizar una asignatura
+        public async Task<int> Editar_Usuario(Usuarios usuarios) //metodo para actualizar una asignatura
         {
-           
+
             var user = _context.Usuarios.Where(x => x.Cedula == usuarios.Cedula).FirstOrDefault(); //verifica si existe una institucion
 
             //si solo cambia el nombre de usuario
-            if (usuarios.Usuario!=null && usuarios.Contraseña==null) // si existen nombre de institucion y nombre de usuario, verifica que sea el mismo a actualizar
+            if (usuarios.Usuario != null && usuarios.Contraseña == null) // si existen nombre de institucion y nombre de usuario, verifica que sea el mismo a actualizar
             {
                 //Verifica que no exista el nombre de usuario
                 var nombre = _context.Usuarios.Where(x => x.Usuario == usuarios.Usuario).FirstOrDefault();
 
-                if (nombre==null)// si no existe el nombre, actualiza
+                if (nombre == null)// si no existe el nombre, actualiza
                 {
                     user.Usuario = usuarios.Usuario;
                     _context.Update(user);
@@ -181,6 +181,56 @@ namespace Redes_De_Solidaridad.Controllers
                 user.Contraseña = usuarios.Contraseña;
                 _context.Update(user);
                 await _context.SaveChangesAsync(); //guarda
+                return 1;
+            }
+        }
+
+        [HttpPut("editarinstitucion")]
+        public async Task<int> Editar_Institucion(UsuarioInstitucion institucion) //metodo para actualizar una asignatura
+        {
+
+            //busca el usuario segun el id de la institucion
+            var user = _context.UsuariosInstituciones.Where(x => x.IdInstitucion == institucion.Id).FirstOrDefault(); //verifica si existe una institucion
+
+            //busca el usuario la institucion
+            var institucio = _context.Institucion.Where(x => x.Id == institucion.Id).FirstOrDefault();
+
+            //si solo cambia el nombre de usuario
+            if (institucion.Contraseña == null) // si existen nombre de institucion y nombre de usuario, verifica que sea el mismo a actualizar
+            {
+                //Verifica que no exista el nombre de usuario
+                var nombre = _context.UsuariosInstituciones.Where(x => x.Usuario ==institucion.Usuario).FirstOrDefault();
+
+                if (nombre == null)// si no existe el nombre, actualiza
+                {
+                    user.Usuario = institucion.Usuario;
+                    _context.Update(user);
+                    await _context.SaveChangesAsync(); //guarda
+
+                    institucio.Direccion = institucion.Direcccion;
+                    institucio.Nombre = institucion.Institucion;
+                    _context.Update(institucio);
+                    await _context.SaveChangesAsync(); //guarda
+
+                    return 1;
+
+                }
+                else // de lo contrario retorna 0
+                    return 0;
+
+            }
+            else // si hay cambios en la contraseña
+            {
+                user.Usuario = institucion.Usuario;
+                user.Contraseña = institucion.Contraseña;
+                _context.Update(user);
+                await _context.SaveChangesAsync(); //guarda
+
+                institucio.Direccion = institucion.Direcccion;
+                institucio.Nombre = institucion.Institucion;
+                _context.Update(institucio);
+                await _context.SaveChangesAsync(); //guarda
+
                 return 1;
             }
         }
